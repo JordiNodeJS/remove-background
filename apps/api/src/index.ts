2cc210f24d1cd9674ed887d7c8ad9b3bf6c6bbf3
JordiNodejs <jordi.nodejs@gmail.com>
2025-05-09 17:46:24 +0200

    fix: parse PORT environment variable as an integer and specify host for app.listen

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,8 @@
 import { createApp } from "./app";
 
 const app = createApp();
-const port = process.env.PORT || 3001;
+const port = parseInt(process.env.PORT || "3001", 10);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Servidor Express ejecutándose en el puerto ${port}`);
 });
