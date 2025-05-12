49e6aeed34478884cc15b3a283a18f29551ba053
JordiNodejs <jordi.nodejs@gmail.com>
2025-05-12 14:11:01 +0200

    feat: add .env.example for configuration and update API URL reference in next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/ext/:path*",
-        destination: process.env.API_URL
-          ? `${process.env.API_URL}/:path*`
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
           : "http://localhost:3001/:path*",
       },
     ];
